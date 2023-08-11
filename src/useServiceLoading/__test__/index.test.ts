8eb62cf3e4aad0e2b2e3e05ea5d751af28c09507
小撸 <lulin666000@gmail.com>
2023-08-11 12:25:34 +0800

    fix: 单测复制错误修复，添加 pub docs

--- src/useServiceLoading/__test__/index.test.ts
+++ src/useServiceLoading/__test__/index.test.ts
@@ -1,8 +1,8 @@
 import { describe, it, expect } from 'vitest';
-import { useService } from '../index';
+import { useServiceLoading } from '../index';
 
-describe('useService', () => {
-  it('useService service ok', () => {
-    expect(useService).toBeDefined();
+describe('useServiceLoading', () => {
+  it('useServiceLoading service ok', () => {
+    expect(useServiceLoading).toBeDefined();
   });
 });
